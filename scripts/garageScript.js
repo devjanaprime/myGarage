98fa5749328fbe4b3dbf5a7ed624593fca7bdb6a
devjanaprime <dev@primeacademy.io>
2016-08-16 13:54:23 -0500

    added basic user input fields

--- scripts/garageScript.js
+++ scripts/garageScript.js
@@ -3,28 +3,23 @@ console.log( 'garageScript.js sourced' );
 var myCars=[];
 
 var addCar = function( year, make, model, picURL, description ){
-  console.log( 'in addCar:', year, make, model, picURL, description );
+  // converted to use user inputs from 0.1
+  console.log( 'in addCar' );
   // create new car Object
   var newCar={
-    year: year,
-    make: make,
-    model: model,
-    picURL: picURL,
-    description: description
+    year: document.getElementById( 'yearIn' ).value,
+    make: document.getElementById( 'makeIn' ).value,
+    model: document.getElementById( 'modelIn' ).value,
+    picURL: document.getElementById( 'picURLIn' ).value,
+    description: document.getElementById( 'descriptionIn' ).value
   }; // end new car object
   // push into array
   myCars.push( newCar );
+  // show cars
+  showCars();
 }; // end addCar
 
 var showCars = function(){
   console.log( 'in showCars');
   console.log( myCars );
 }; // end showCars
-
-addCar( 1988, 'Mercedes', '300e', 'http://mercedesforum.com/forum/members/arctic1-41283-albums-my-benz-s-59-picture-1988-300e-diamond-blue-metallic-my-first-320.jpg', 'It had headlight windshield wipers' );
-addCar( 1992, 'Ford', 'E350', 'https://upload.wikimedia.org/wikipedia/commons/8/88/83-91_Ford_Club_Wagon.jpg', 'Former work van for HVAC company. Added orange shag carpet, wood paneling, custom lights, and named it "Hope"' );
-addCar( 2000, 'Dodge', 'Ram Van', 'http://gomotors.net/photos/1e/7e/2000-dodge-ram-van-1500-maxi-van-in-corona-california-for-sale_46677.jpg', 'Purple carpet with wood paneling.' );
-addCar( 2005, 'Chrysler', 'PT Cruiser', 'http://images.gtcarlot.com/pictures/14315360.jpg', 'You can remove the seats!' );
-addCar( 2012, 'Kia', 'Soul', 'http://www.kiasoulforums.com/attachments/owners-photo-gallery/605d1248154024-dune-soul-soul-1.jpg', 'Could change the color of the interior lights. Though an SUV, was easy to park');
-addCar( 2015, 'Volkswagon', 'Jetta', 'http://images.dealerfire.com/media/websites/2000/content/2015-Volkswagen-Jetta-B2.jpg?s=89164', 'Got the turbo model!' );
-showCars();
