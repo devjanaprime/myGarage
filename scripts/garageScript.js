0df0f231e3992f544aa97b2fd4cbf27f63e2b417
devjanaprime <dev@primeacademy.io>
2016-08-16 15:21:00 -0500

    added basic styling to output. now clears inputs on addition

--- scripts/garageScript.js
+++ scripts/garageScript.js
@@ -17,26 +17,30 @@ var addCar = function( year, make, model, picURL, description ){
   myCars.push( newCar );
   // show cars
   showCars();
+  // clear inputs
+  document.getElementById( 'yearIn' ).value='';
+  document.getElementById( 'makeIn' ).value='';
+  document.getElementById( 'modelIn' ).value='';
+  document.getElementById( 'picURLIn' ).value='';
+  document.getElementById( 'descriptionIn' ).value='';
 }; // end addCar
 
 var showCars = function(){
   console.log( 'in showCars');
-  // upated from ver 0.2 to output an unordered list of cars in garage to DOM
-  // not just a cheesy console.log...
-  // empty the ul
+  // upated from ver 0.3 to output the make/model/year, car image, and description to a div instead of as li in a ul
+  // clear the output div
   document.getElementById( 'carsList' ).innerHTML='';
   // for every car in the array...
   for( var i=0; i< myCars.length; i++ ){
-    // assemble an output text line
-    var outputText = myCars[ i ].year + ' ' + myCars[ i ].make + ' ' + myCars[ i ].model + ': ' + myCars[ i ].description;
-    // create new list item
-    var newListItem = document.createElement( 'li' );
-    // create new text node with the output text
-    var newTextNode = document.createTextNode( outputText );
-    // append text node to list item
-    newListItem.appendChild( newTextNode );
-    // append new list item to existing ul
-    document.getElementById( 'carsList' ).appendChild( newListItem );
+    // assemble an output text line for the year/make model
+    var outputText0 = '<h3>' + myCars[ i ].year + ' ' + myCars[ i ].make + ' ' + myCars[ i ].model + '</h3>';
+    // assemble an output text line for the pic if there is one
+    var outputText1 = '<img src="' + myCars[ i ].picURL + '">';
+    // assemble an output text line for the description
+    var outputText2 = '<p>' + myCars[ i ].description + '<p>';
+    // append these all to the body
+    var outputText = '<div>' + outputText0 + outputText1 + outputText2 + '</div>';
+    document.getElementById('carsList').innerHTML+=outputText;
   }; // end for
 
 }; // end showCars
