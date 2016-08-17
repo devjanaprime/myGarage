fe5956ef87c1004ab8f593589ebbc89143118fd6
devjanaprime <dev@primeacademy.io>
2016-08-17 05:54:49 -0500

    basic JQuery conversion

--- scripts/garageScript.js
+++ scripts/garageScript.js
@@ -2,41 +2,46 @@ console.log( 'garageScript.js sourced' );
 // myCars array
 var myCars=[];
 
-var addCar = function( year, make, model, picURL, description ){
-  // converted to use user inputs from 0.1
-  console.log( 'in addCar' );
-  // create new car Object
-  var newCar={
-    year: document.getElementById( 'yearIn' ).value,
-    make: document.getElementById( 'makeIn' ).value,
-    model: document.getElementById( 'modelIn' ).value,
-    picURL: document.getElementById( 'picURLIn' ).value,
-    description: document.getElementById( 'descriptionIn' ).value
-  }; // end new car object
-  // push into array
-  myCars.push( newCar );
-  // show cars
-  showCars();
-}; // end addCar
+// converted to JQuery from ver 0.3 of My Garage
 
-var showCars = function(){
-  console.log( 'in showCars');
-  // upated from ver 0.2 to output an unordered list of cars in garage to DOM
-  // not just a cheesy console.log...
-  // empty the ul
-  document.getElementById( 'carsList' ).innerHTML='';
-  // for every car in the array...
-  for( var i=0; i< myCars.length; i++ ){
-    // assemble an output text line
-    var outputText = myCars[ i ].year + ' ' + myCars[ i ].make + ' ' + myCars[ i ].model + ': ' + myCars[ i ].description;
-    // create new list item
-    var newListItem = document.createElement( 'li' );
-    // create new text node with the output text
-    var newTextNode = document.createTextNode( outputText );
-    // append text node to list item
-    newListItem.appendChild( newTextNode );
-    // append new list item to existing ul
-    document.getElementById( 'carsList' ).appendChild( newListItem );
-  }; // end for
+$( document ).ready( function(){
+  console.log( 'in JQUERY' );
 
-}; // end showCars
+  // target the addCarButton by it's ID (thus the #)
+  $( '#addCarButton' ).on( 'click', function(){
+    console.log( 'in addCarButton click');
+    // create new car Object
+    var newCar={
+      year: document.getElementById( 'yearIn' ).value,
+      make: document.getElementById( 'makeIn' ).value,
+      model: document.getElementById( 'modelIn' ).value,
+      picURL: document.getElementById( 'picURLIn' ).value,
+      description: document.getElementById( 'descriptionIn' ).value
+    }; // end new car object
+    // push into array
+    myCars.push( newCar );
+    // show cars
+    showCars();
+  }); // end addCarButton click
+
+  var showCars = function(){
+    console.log( 'in showCars');
+    // upated from ver 0.2 to output an unordered list of cars in garage to DOM
+    // not just a cheesy console.log...
+    // empty the ul
+    $( '#carsList' ).empty();
+    // for every car in the array...
+    for( var i=0; i< myCars.length; i++ ){
+      // assemble an output text line
+      var outputText = myCars[ i ].year + ' ' + myCars[ i ].make + ' ' + myCars[ i ].model + ': ' + myCars[ i ].description;
+      // create new list item
+      var newListItem = document.createElement( 'li' );
+      // create new text node with the output text
+      var newTextNode = document.createTextNode( outputText );
+      // append text node to list item
+      newListItem.appendChild( newTextNode );
+      // append new list item to existing ul
+      $( '#carsList' ).append( newListItem );
+    }; // end for
+  }; // end showCars
+});
